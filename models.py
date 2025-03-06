92b3cc5453a80a2ec955dad60cc1abcf45137e44
mezue30000 <mezue3000>
2025-03-06 03:16:54 +0100

    modified models

--- models.py
+++ models.py
@@ -5,17 +5,16 @@ from typing import Optional, List
 from datetime import datetime, timezone
 
 
-
-
 # create user model
 class User(SQLModel, table= True):
     user_id: Optional[int] = Field(default=None, primary_key=True)
-    first_name: str = Field(..., max_length=25, index=True)
-    last_name: str = Field(..., max_length=25)
-    email: str = Field(..., max_length=50, unique=True)
-    password_hash: str = Field(...)
-    country: str = Field(..., max_length=25)
-    city: str = Field(..., max_length=25)
+    first_name: str = Field(max_length=25, nullable=True)
+    last_name: str = Field(max_length=25, nullable=False)
+    username: str = Field(max_length=50, nullable=False, index=True) 
+    email: str = Field(max_length=50, unique=True, nullable=False)
+    password_hash: str = Field(nullable=False)
+    country: str = Field(max_length=25, nullable=False)
+    city: str = Field(max_length=25, nullable=False)
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
     # add relationship
     posts : List[Post] = Relationship(back_populates= "user")
@@ -24,11 +23,12 @@ class User(SQLModel, table= True):
 # create post model 
 class Post(SQLModel, table=True):
     post_id: Optional[int] = Field(default=None, primary_key=True)
-    title: str = Field(..., max_length=125, index=True)
-    content: str = Field(..., max_length=450)
+    title: str = Field(max_length=125, index=True, nullable=False)
+    content: str = Field(max_length=450, nullable=False)
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
     updated_at: datetime = Field(
         default_factory=lambda: datetime.now(timezone.utc), sa_column_kwargs= {'onupdate': func.now()},nullable=False)
+    # add foreign key
     user_id: int = Field(
         foreign_key= "user.user_id", sa_relationship_kwargs={
         "onupdate": "CASCADE", "ondelete": "NO ACTION"})
@@ -40,14 +40,15 @@ class Post(SQLModel, table=True):
 # create comment model
 class Comment(SQLModel, table=True):
      comment_id: Optional[int] = Field(default=None, primary_key=True)
-     content: str = Field(..., max_length=450, index=True)
+     content: str = Field(max_length=450, index=True, nullable=False)
      created_at: datetime = Field(
         default_factory=lambda: datetime.now(timezone.utc), nullable=False)
      
      updated_at: datetime = Field(
         default_factory=lambda: datetime.now(timezone.utc), sa_column_kwargs={"onupdate": func.now()}, nullable=False)
-     
+    #  add foreign key
      post_id: int = Field(
         foreign_key= "post.post_id", sa_relationship_kwargs={
         "onupdate": "CASCADE", "ondelete": "NO ACTION"})
+    #  add relationship
      post: Post = Relationship(back_populates= "comments")
