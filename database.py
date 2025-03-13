7e49b32d0d2062a17fde0b88112bd044932b023d
mezue30000 <mezue3000>
2025-03-13 05:59:59 +0100

    created database.py(engine, get_db, create_tables

--- database.py
+++ database.py
@@ -1,6 +1,31 @@
-from sqlmodel import SQLModel, create_engine, Session
+# import dependencies
+from sqlmodel import SQLModel
+import aiomysql
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+import models
+import asyncio
 
 
-# create engine
-url = "mysql+aiomysql://blogger_app:angelcode1234567@127.0.0.1:3306/my_blog"
-engine = create_engine()
+# create asynchronous engine
+database_url = "mysql+aiomysql://blogger_app:angelcode1234567@127.0.0.1:3306/my_blog"
+async_engine = create_async_engine(database_url, echo= True) 
+
+
+# create an async session factory
+AsyncSessionLocal = async_sessionmaker(bind=async_engine, expire_on_commit= False, autoflush= False) 
+
+
+# function to get a session
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield  session
+        
+        
+# function to create tables
+async def create_tables():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+        
+
+if __name__ == "__main__":
+    asyncio.run(create_tables()) 
